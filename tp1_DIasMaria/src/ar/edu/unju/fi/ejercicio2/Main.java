4acf644ed03ff3b9a1445ffa6af8ae90e97333dc
User <User@DESKTOP-JAUP7AC>
2024-04-09 14:30:57 -0300

    Se agrega el ejercicio2 del tp_1 con su resolucion correspondiente

--- tp1_DIasMaria/src/ar/edu/unju/fi/ejercicio2/Main.java
+++ tp1_DIasMaria/src/ar/edu/unju/fi/ejercicio2/Main.java
@@ -3,22 +3,22 @@ package ar.edu.unju.fi.ejercicio2;
 
 public class Main {
 	
-
 	public static void main(String[] args) {
 		
 		String nompais = "Argentina";
 	    int edadP = 19;
 		double alturaE = 23.8;
 		double producto = 50.99;
-	    String nro = "3882910320";
+	    String nrotell = "3882910320";
 		double cos = 0.5;
 		
 		System.out.println("Nombre de Pais: " + nompais);
 		System.out.println("Edad Persona: " + edadP + " Años");
 		System.out.println("Altura Edificios: " + alturaE + "m");
 		System.out.println("Precio Producto: " + producto + "$");
-		System.out.println("Numero de telefono: " + nro);
+		System.out.println("Numero de telefono: " + nrotell);
 		System.out.println("Calculo coseno : " + cos);
+		
 	}
 
 }
