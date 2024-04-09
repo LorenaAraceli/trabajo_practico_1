421e890318d2426f4eb3489935e5ec2174dae111
User <User@DESKTOP-JAUP7AC>
2024-04-09 10:26:48 -0300

    Se crea la clase Main 1 para el ejercicio 1 del tp_1, con su codigo correspondiente.

---
+++ tp1_DIasMaria/src/ar/edu/unju/fi/ejercicio1/Main.java
@@ -0,0 +1,20 @@
+package ar.edu.unju.fi.ejercicio1;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		int v1 = 8;
+		int v2 = 5;
+		int v3 = 1;
+		int v4 = 20;
+		int v5 = 9;
+		int suma;
+		double promedio; 
+		
+		suma = v1 + v2 + v3 + v4 + v5;
+		promedio = (double)suma/5;
+		System.out.println("El promedio es: " + promedio);
+		
+	}
+
+}
