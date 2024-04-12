46be9b1ec95c7657bad84ab36084694f1544c786
User <User@DESKTOP-JAUP7AC>
2024-04-12 18:19:27 -0300

    se agrega el paquete con espacios. ejercicio11 del tp1

--- tp1_DIasMaria/src/ar/edu/unju/fi/ejercicio11/Main.java
+++ tp1_DIasMaria/src/ar/edu/unju/fi/ejercicio11/Main.java
@@ -1,10 +1,17 @@
 package ar.edu.unju.fi.ejercicio11;
 
+
 public class Main {
+    public static void main(String[] args) {
+        int i = 2;
+        int j = 40;
+        int num = 80;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+        
+        for (;num>=0 ; i++,j--) {
+        	 System.out.print(" " + num);
+        	 num = num + (j-i-1);
+          
+        }
+    }
 }
